4788e07e497ed8126aab544543bb31b8bf4fd659
bilalehmad <bilalahmed.36@outlook.com>
2023-07-12 18:54:27 +0500

    fix router

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import {  } from "next/navigation";
+import {  useRouter} from "next/navigation";
 
 const Dashboard = () => {
     const {data: session} = useSession();
