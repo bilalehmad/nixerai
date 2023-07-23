2db83b7a50a00e85a04a819ae8661967922f7e51
bilalehmad <bilalahmed.36@outlook.com>
2023-07-23 06:25:18 +0500

    fix json error on ai-tool page adding async

--- app/ai-tool/page.jsx
+++ app/ai-tool/page.jsx
@@ -9,8 +9,8 @@ const fetchFirstPosts = async () => {
   return data;
 }
 
-const AITool =  () => {
-  var data = fetchFirstPosts();
+const AITool =  async() => {
+  var data = await fetchFirstPosts();
   return (
     <section className="w-full flex-center flex-col">
     <h1 className="head_text text-center">
