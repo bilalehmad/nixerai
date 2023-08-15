d42d9c4d466141d24a3c740f1c3a8621178400b3
bilalehmad <bilalahmed.36@outlook.com>
2023-08-15 13:47:02 +0500

    fix fetch issues

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -9,8 +9,8 @@ export const GET = async (request) => {
     try {
         await connectToDB()
 
-        const blogs = await Blogs.find({}).populate('auther')
-
+        const blogs = await Blogs.find({}).populate("auther")
+        console.log(blogs)
         return new Response(JSON.stringify(blogs), { status: 200 })
     } catch (error) {
         return new Response("Failed to fetch all Pakages", { status: 500 })
--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import Timestamp from './Timestamp';
 
 const BlogList = ({data}) => {
+    console.log(data)
     const [posts, setPosts] = useState(data);
     const [link, setLink] = useState('')
     const router = useRouter();
@@ -15,7 +16,7 @@ const BlogList = ({data}) => {
   return (
     <>
         {posts && posts.map((value,key) =>  (
-            <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <article key={key} className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <div className="flex justify-between items-center mb-5 text-gray-500">
                 <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                     {value.subject == "Article" ? (
