6048c9a547e3a8dd0340e28016cf6e769737a680
bilalehmad <bilalahmed.36@outlook.com>
2023-08-26 15:51:28 +0500

    remove session props from provider console

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -3,7 +3,6 @@ import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes';
 
 const Provider = ({children}) => {
-  console.log(session,"usersession")
   return (
     
     <SessionProvider>
