1d3c701a7f1b5dd8383761f49c03526610185482
bilalehmad <bilalahmed.36@outlook.com>
2023-08-26 19:39:22 +0500

    add loader again 89

--- app/ai-tool/loading.jsx
+++ app/ai-tool/loading.jsx
@@ -1,6 +1,11 @@
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return (
-        <section className='w-full max-w-full h-screen flex justify-center mt-24'>Loading...</section>
-    )
-  }
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div role="status" className='h-screen flex '>
+    <div className='mt-40 text-gray-800 items-center justify-center'>Loading...</div>
+</div>
+  )
+}
+
+export default Loading
