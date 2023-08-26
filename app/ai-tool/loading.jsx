f3286eb83c082a26e0b6392244b3fd0783bbc6bf
bilalehmad <bilalahmed.36@outlook.com>
2023-08-26 19:30:33 +0500

    add loader again

---
+++ app/ai-tool/loading.jsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <section className='w-full max-w-full h-screen flex justify-center mt-24'>Loading...</section>
+    )
+  }
